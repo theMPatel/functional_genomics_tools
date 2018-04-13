c6747ea8ef20a264f81fd30f872d2190ba8f1553
Milan Patel <nft9@cd.gov>
2018-04-13 10:14:45 -0400

    Adding new stxfinder functionality in

---
+++ ce/bin/genotyping/ecoli_stxtypefinder.py
@@ -0,0 +1,267 @@
+###################################################################
+#
+# Detects the various stx gene variants for Escherichia
+# You can use this for basically anything you want to
+# implement into the Calculation Engine
+#
+# Author: Hannes Pouseele
+# Contact: hannes_pouseele@applied-math.com
+# Version 1.0
+#
+###################################################################
+
+import os
+import json
+
+from tools.environment import (
+    log_algo_version,
+    log_message,
+    write_results,
+    time_now,
+    valid_dir,
+)
+
+from tools.tools import (
+    parse_fasta,
+    is_fasta
+)
+
+from tools.dbinfo import (
+    DbInfo,
+    LocusInfo,
+    notes_parser
+)
+
+from .rb_detection import Main2 as ReadFindPresenceAbsence
+from .rb_detection import __version__
+from collections import namedtuple
+
+def ValidateInput(settings):
+    pass
+
+SeqInfo = namedtuple('SeqInfo', ['locus', 'allele', 'accession', 'sequence', 'other'])
+StxSeqInfo = namedtuple('StxSeqInfo', SeqInfo._fields + ('stx', 'variant'))
+
+def ParserForSeq(header, seq, separator=':'):
+    parts = header.split('|')[0].split(separator)
+    while len(parts) < 2:
+        parts.append('')
+
+    stx = parts[0][:4]
+    variant = parts[0][-1]
+    return StxSeqInfo(parts[0], '', parts[1], seq, '{}:{}'.format(stx, variant), stx, variant)
+
+class StxDbInfo(object):
+    def __init__(self, folder=None, seqParser=ParserForSeq, notesParser=notes_parser, acceptFunc=None):
+        self._references = {}
+        self._notes = {}
+        self._sequences = {}
+
+        if folder is not None:
+            self.Load(folder, seqParser, notesParser
+                      , acceptFunc)
+
+    def Load(self, folder, seqParser, notesParser, acceptFunc):
+
+        def MyAcceptFunc(seqInfo):
+            return True
+
+        flName = os.path.join(folder, "stx1-all.fasta")
+        self.LoadFile(flName, seqParser, notesParser, MyAcceptFunc)
+        flName = os.path.join(folder, "stx2-no-f-all.fasta")
+        self.LoadFile(flName, seqParser, notesParser, MyAcceptFunc)
+        flName = os.path.join(folder, "stx2-f-all.fasta")
+        self.LoadFile(flName, seqParser, notesParser, MyAcceptFunc)
+
+        seqs = parse_fasta(os.path.join(folder, 'stx_consensus.fasta'))
+        self._references = {
+            k: SeqInfo(k, '', '', v, '') for k, v in seqs.iteritems()
+        }
+
+    def LoadFile(self, flName, seqParser, notesParser, acceptFunc):
+        seqs = parse_fasta(flName)
+        for header, seq in seqs.iteritems():
+            info = seqParser(header, seq)
+            if acceptFunc and not acceptFunc(info):
+                continue
+
+            alleleId = '{}_{}'.format(info.locus, info.allele) if info.allele else info.locus
+            idx = 2
+            while alleleId in self._sequences:
+                alleleId = '{}_{}-{}'.format(info.locus, info.allele, idx)
+                idx += 1
+            if alleleId not in self._sequences:
+                self._sequences[alleleId] = info
+
+    def GetRefSeqId(self, seqId):
+        if 'stx2' in seqId:
+            if 'f' not in seqId:
+                return 'consensus-stx2-no-f'
+            else:
+                return 'consensus-stx2-f'
+        else:
+            return 'consensus-stx1'
+
+    def GetSeq(self, seqId):
+        return self._sequences[seqId].sequence.replace('-', '')
+
+    def GetAlignedSeq(self, seqId):
+        return self._sequences[seqId].sequence
+
+    def GetAlignedRefSeq(self, seqId):
+        return self._sequences[seqId].sequence
+
+    def GetRefSeq(self, seqId):
+        return self._sequences[seqId].sequence
+
+    def Allele(self, seqId):
+        return self._sequences[seqId]
+
+    def Locus(self, locus):
+        return self._notes.get(locus, None)
+
+    def ExportToFasta(self, flName):
+        valid_dir(os.path.split(flName)[0])
+        with open(flName, 'w') as oFile:
+            for seqId, seqInfo in self._sequences.iteritems():
+                oFile.write('>{}|{}\n{}\n'.format(seqId, len(seqInfo.sequence), seqInfo.sequence))
+
+    def ExportRefsToFasta(self, flName):
+        valid_dir(os.path.split(flName)[0])
+        with open(flName, 'w') as oFile:
+            for seqId, seqInfo in self._sequences.iteritems():
+                oFile.write('>{}\n{}\n'.format(seqId, seqInfo.sequence))
+
+    @property
+    def RefSequences(self):
+        return self._references
+
+    @property
+    def Sequences(self):
+        return self._sequences
+
+    def ExportRefsToFasta(self, flName):
+        valid_dir(os.path.split(flName)[0])
+        with open(flName, 'w') as oFile:
+            for seqId, seqInfo in self._references.iteritems():
+                oFile.write('>{}\n{}\n'.format(seqId, seqInfo.sequence))
+
+def main(settings, env):
+    # write basic logging info
+
+    log_message('Starting running reads based stx subtyping algorithm', 1)
+
+    log_algo_version(
+        algo_version=__version__,
+        settings=settings,
+        env=env
+    )
+
+    # perform input data validation
+    ValidateInput(settings)
+
+    # load database info
+    database_path = env.get_sharedpath(settings.database)
+
+    log_message('Database path found at: {}'.format(
+        database_path), 2
+    )
+
+    dbInfo = StxDbInfo(database_path, ParserForSeq)
+
+    # run the finder
+    log_message("Determining fixed-position base calls...")
+    if 'query_reads' in settings and settings.query_reads:
+
+        mappingSettings = {
+            'Bowtie2': {
+                '--reorder': None,  # to avoid bowtie2 from crashing when wrongly doing multithreaded output
+                '--local': None,  # perform local mapping
+                '--sensitive-local': None,  # default setting: sensitive
+                # '--rdg': '3,1', #make it gappy-er
+                # '--score-min': 'L,90,0', #adjust the minimum scoring function to f(readlen) = A + B*readlen
+                '--no-unal': None,  # avoid writing unaligned reads in the sam file
+                '--all': None,  # report all alignments
+            },
+            'BAMCoveragePrinter': {
+                'output_raw_coverages': None,
+                'count_multiple_alignments': None,
+            },
+            # 'samtools' : {}
+            'LoadCoverage': True,
+        }
+
+        results = ReadFindPresenceAbsence(
+            dbInfo,
+            settings.query_reads,
+            settings.reads.percent_identity,
+            settings.reads.min_coverage,
+            settings.reads.min_relative_coverage,
+            mappingSettings,
+            env.threads,
+            env
+        )
+    # Classic, the below function is not defined anywhere...
+    # else:
+    #     results = FindPositionsOnAssembly(
+    #         dbInfo,
+    #         settings.query,
+    #         settings.assembly.percentIdentity,
+    #         settings.assembly.minRelCoverage,
+    #         env
+    #     )
+
+    # write this into a file
+    allresults = {
+        'date': time_now(),
+        'genotypes': results
+    }
+
+    write_results('StxTypeFinder_genotypes.json', json.dumps(allresults))
+
+    # parse position information into stx types
+    log_message("Performing stx calling on fixed-position base calls...")
+    stxTypeResults = {}
+    if 'stx-aligned_1' in results:
+        stxTypeResults = {
+            tpe: sum(any(c in results['stx-aligned_1'][p] for c in call[p]) for p in call)
+            for tpe, call in dbInfo.Calls.iteritems()
+        }
+
+    write_results('StxTypeFinder_stxtypes.json', json.dumps(stxTypeResults))
+
+
+if __name__ == '__main__':
+    from collections import namedtuple
+    Settings = namedtuple('Settings', [
+        'localDir',
+        'resultsDir',
+        'sharedDir',
+        'toolsDir',
+        'query',
+        'percentIdentity',
+        'minRelCoverage',
+        'minOverlapForMerge',
+        'searchForFragments'
+    ])
+
+    settings = Settings(
+        r'c:\users\hannes\temp\test_genotyping\local',
+        r'c:\users\hannes\temp\test_genotyping',
+        r'C:\tfs\Internal\Test\Hannes\scripts\genotyping\am_databases',
+        r'C:\Program Files\Applied Maths\BioNumerics 7.6 bleeding edge\SequenceData\Blast',
+        # r'c:\users\hannes\temp\test_genotyping\Escherichia_coli_O157H7_Sakai.fasta.gz',
+        # r'c:\users\hannes\temp\test_genotyping\Shigella_boydii_965_58.fasta.gz',
+        r'c:\users\hannes\temp\test_genotyping\query.fasta',
+        0.9,
+        0.6,
+        0.9,
+        True
+    )
+
+    Main(settings)
+
+
+
+
+
---
+++ ce/bin/genotyping/rb_detection.py
@@ -0,0 +1,147 @@
+###################################################################
+#
+# Environment specifics for the genotyping algorithm
+# You can use this for basically anything you want to
+# implement into the Calculation Engine
+#
+# Author: Hannes Pouseele
+# Contact: hannes_pouseele@applied-maths.com
+# Version 1.0
+#
+###################################################################
+
+__version__ = '0.1.0'
+
+import os
+from tools.bowtie import RunBowtie2
+
+def CompareCov(cov, seq, start, stop, reflen, minCov):
+    return (1.0 * sum(cov.GetCovTot(i) >= minCov for i in xrange(start, stop) if seq[i] in 'ACGT')) / (
+                1.0 * (stop - start))
+
+def CompareSeq(cov, seq, start, stop, reflen, minCov):
+    return (1.0 * sum(cov.GetCov(i, seq[i]) >= minCov for i in xrange(start, stop) if seq[i] in 'ACGT')) / (
+                1.0 * (stop - start))
+
+def Main(dbInfo, readFiles, percentIdentity, minCovDepth, minCovRef, mappingSettings, nThreads, env):
+    bowtie2Results = RunBowtie2(dbInfo, readFiles, mappingSettings, nThreads, env)
+
+    # determine coverages
+    results = []
+    for seqId, seqInfo in dbInfo.Sequences.iteritems():
+
+        refSeqId = dbInfo.GetRefSeqId(seqId)
+        alignedRefSeq = dbInfo.GetAlignedRefSeq(refSeqId)
+        refSeq = dbInfo.GetRefSeq(refSeqId)
+
+        bowtie2Result = bowtie2Results.GetCoverage(refSeqId)
+
+        assert bowtie2Result is not None, "Hmm are we missing a reference?"
+
+        start = alignedRefSeq.find('-')
+        if start == -1: start = 0
+        stop = alignedRefSeq.rfind('-')
+        if stop == -1: stop = len(alignedRefSeq)
+        refLen = len(alignedRefSeq) - alignedRefSeq.count('-')
+        covScore = CompareCov(bowtie2Result, alignedRefSeq, start, stop, refLen, minCovDepth)
+        seqIdScore = CompareSeq(bowtie2Result, alignedRefSeq, start, stop, refLen, minCovDepth)
+        isPresent = covScore >= minCovRef
+        isCloseEnough = seqIdScore >= percentIdentity
+        if isPresent and isCloseEnough:
+            results.append(
+                {'seqId': seqIdScore, 'cov': covScore, 'seqInfo': seqInfo}
+            )
+
+            # report this out
+    return results
+
+# this will find the desired sequences in an exhaustive way
+def Main2(dbInfo, readFiles, percentIdentity, minCovDepth, minCovRef, mappingSettings, nThreads, env):
+    bowtie2Results = RunBowtie2(dbInfo, readFiles, mappingSettings, nThreads, env)
+
+    # determine coverages
+    results = []
+
+    seqDone = {}
+    for cntRefs, (seqId, seqInfo) in enumerate(dbInfo.Sequences.iteritems()):
+
+        if cntRefs % 10 == 0:
+            print cntRefs
+
+        alignedSeq = dbInfo.GetAlignedSeq(seqId)
+        ungappedSequence = dbInfo.GetSeq(seqId)
+
+        # mapping is done against the reference sequences
+        # so we need to translate the seqId into the right refSeqId
+        # to be able to pick up the coverage vector
+        refSeqId = dbInfo.GetRefSeqId(seqId)
+        bowtie2Result = bowtie2Results.GetCoverage(refSeqId)
+
+        assert bowtie2Result is not None, "Hmm are we missing a reference?"
+
+        sampleLen = len(alignedSeq)
+        covInfo = [
+            {b: bowtie2Result.GetCov(i, b) for b in 'ACGT-' if bowtie2Result.GetCov(i, b) >= minCovDepth}
+            for i in xrange(sampleLen)
+        ]
+
+        # try finding the sequence in the coverage
+        # dynamic programming kind of walk-through, not very efficient though
+        toCheck = [(0, 0, 0, [])]
+        found = []
+        minPenalty = 10  # max number of mismatches allowed
+        iterCount = 0
+        maxDiag = 5
+        posDone = set()
+        while toCheck:
+            # dummy check to avoid infinite loops
+            if iterCount > 1.1 * (2 * maxDiag + 1) * sampleLen:
+                raise RuntimeError("Problem with species identification - infinite loop")
+
+            # handle a stepstone iteration in the path
+            sampleIdx, refIdx, penalty, covlist = toCheck.pop()
+            posDescr = '{}|{}'.format(sampleIdx, refIdx)
+            if posDescr in posDone: continue
+
+            posDone.add(posDescr)
+
+            iterCount += 1
+            if refIdx + 1 == len(ungappedSequence):  # we are at the end of the sequence
+                minPenalty = min(minPenalty,
+                                 penalty)  # adjust min penalty to force an early stop for non-promising paths
+                found.append([penalty, covlist])
+            else:
+                # check if we have a base
+                # if seqInfo._IsReal(ungappedSequence[refIdx], sampleIdx): #slower
+                if ungappedSequence[refIdx] in covInfo[sampleIdx]:
+                    # if so: consume it
+                    if sampleIdx + 1 < sampleLen:
+                        toCheck.append((sampleIdx + 1, refIdx + 1, penalty,
+                                        covlist + [covInfo[sampleIdx][ungappedSequence[refIdx]]]))
+                else:
+                    # if not: add a mismatch
+                    if sampleIdx + 1 < sampleLen and (penalty + 1 <= minPenalty):
+                        toCheck.append((sampleIdx + 1, refIdx + 1, penalty + 1, covlist + [-1]))
+
+                # add a gap if that is possible
+                if '-' in covInfo[sampleIdx]:
+                    if sampleIdx + 1 < sampleLen:  # and abs(refIdxMap[sampleIdx]-refIdx)<maxDiag:
+                        toCheck.append((sampleIdx + 1, refIdx, penalty, covlist + [covInfo[sampleIdx]['-']]))
+        if found:
+            # determine best-scoring path
+            minIdx = min(enumerate(found), key=lambda x: x[1])[0]
+            # grab coverage
+            sampleCov = found[minIdx][1]
+            # calculate abundance
+            diffPosns = [i for i in xrange(len(sampleCov)) if bowtie2Result.IsAmbiguous(i, minCovDepth)]
+            abundance = sum(sampleCov[i] for i in diffPosns) / len(diffPosns) if len(diffPosns) > 0 else -1
+            # add prediction
+            results.append(
+                {'penalty': found[minIdx][0], 'abundance': abundance, 'seqInfo': seqInfo}
+            )
+            # keep track of sequence already done
+            seqDone[ungappedSequence] = abundance
+
+            # report this out
+    return results
+
