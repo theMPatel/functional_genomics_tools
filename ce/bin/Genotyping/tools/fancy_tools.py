1b10788c86f8ae48dda3a5ffda9507719daf560d
Milan Patel <nft9@cd.gov>
2018-05-16 15:23:31 -0400

    Added Levenshtein distance algo

--- ce/bin/Genotyping/tools/fancy_tools.py
+++ ce/bin/Genotyping/tools/fancy_tools.py
@@ -81,3 +81,107 @@ class DecisionTree(object):
 
         else:
             return self._tree[branch[0][1]].recurse(branch[1:])
+
+def edit_distance(c, n, matrix=False):
+    c_l = len(c)
+    n_l = len(n)
+
+    if not c_l:
+        return n_l
+
+    if not n_l:
+        return c_l
+        
+    if c_l < n_l:
+        return edit_distance(n, c)
+
+    m = [[0]*(n_l+1) for _ in range(c_l+1)]
+
+    for i in range(c_l+1):
+        m[i][0] = i
+
+    for j in range(n_l+1):
+        m[0][j] = j
+
+    for i in range(c_l):
+
+        for j in range(n_l):
+
+            if c[i] == n[j]:
+
+                m[i+1][j+1] = m[i][j]
+
+            else:
+
+                m[i+1][j+1] = min(
+                    m[i][j],
+                    m[i+1][j],
+                    m[i][j+1]
+                ) + 1
+
+    if matrix:
+        return m
+    else:
+        return m[c_l][n_l]
+
+def build_consensus(c, n):
+    # Get the edit distance_matrix 
+    if not c:
+        return n
+
+    if not n:
+        return c
+
+    m = edit_distance(c, n, matrix=True)
+    
+    # Here we will rebuild the sequence
+    i = len(m)
+    j = len(m[0])
+
+    final = []
+    while i >= 1 and j >= 1:
+
+        v = m[i][j]
+
+        up_left = m[i-1][j-1]
+        left = m[i-1][j]
+        up = m[i][j-1]
+
+        if all(up_left == x for x in [left, up]):
+            i-=1
+            j-=1
+            final.append(n[j])
+        
+        else:
+            m_val = min([up_left, left, up])
+
+            if up_left == m_val:
+
+                i -= 1
+                j -= 1
+
+                if c[i] == n[j]:
+                    final.append(n[j])
+                else:
+                    final.append('N')
+
+            elif left == m_val:
+
+                i-=1
+                final.append('N')
+
+            else:
+                j -= 1
+                final.append('N')
+
+    while i >= 1:
+        final.append('N')
+        i-=1
+
+    while j >= 1:
+        final.append('N')
+        j-=1
+
+    final.reverse()
+
+    return final
